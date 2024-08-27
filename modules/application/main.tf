10aa81c1b5358e11bea08e1a6c1c0b635d917e71
Bhargav Parmar <bhargavparmar7080@gmail.com>
2024-08-27 17:35:09 +0530

    [UPDATE]: added ec2 instance in private vpc and updated security policies

--- modules/application/main.tf
+++ modules/application/main.tf
@@ -6,7 +6,6 @@ resource "tls_private_key" "my_key" {
 resource "aws_key_pair" "ec2-tf" {
   key_name   = "ec2-tf"
   public_key = tls_private_key.my_key.public_key_openssh
-  # public_key = file("/Users/bhargavparmar/.ssh/ec2-tf.pub")
 }
 
 # EC2 in public subnet
@@ -16,7 +15,7 @@ resource "aws_instance" "webserver-pblc-ec2" {
   subnet_id                   = var.pblc-environment-subnet
   associate_public_ip_address = var.enable-pblc-ip-webserver-ec2
   security_groups             = [aws_security_group.webserver-pblc-ec2-sg.id]
-  key_name = aws_key_pair.ec2-tf.key_name
+  key_name                    = aws_key_pair.ec2-tf.key_name
 
   tags = {
     Name = "${var.project}-${var.env}-webserver-ec2"
@@ -34,16 +33,93 @@ resource "aws_security_group" "webserver-pblc-ec2-sg" {
   }
 }
 
-resource "aws_vpc_security_group_ingress_rule" "webserver-pblc-ec2-sg-policy" {
+resource "aws_vpc_security_group_ingress_rule" "webserver-pblc-ec2-ssh-sg-policy" {
   security_group_id = aws_security_group.webserver-pblc-ec2-sg.id
   cidr_ipv4         = var.sg-policy-cidr
   from_port         = 22
   ip_protocol       = "tcp"
   to_port           = 22
   tags = {
-    Name = "${var.project}-${var.env}-ssh-webserver-ec2-sg-policy"
+    Name = "${var.project}-${var.env}-ssh-webserver-ec2-ssh-sg-policy"
   }
-
 }
 
+resource "aws_vpc_security_group_ingress_rule" "webserver-pblc-ec2-allow-all-sg-policy" {
+  security_group_id = aws_security_group.webserver-pblc-ec2-sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 0
+  ip_protocol       = "-1"
+  to_port           = 65535
+  tags = {
+    Name = "${var.project}-${var.env}-ssh-webserver-ec2-allow-all-sg-policy"
+  }
+}
 
+resource "aws_vpc_security_group_egress_rule" "webserver-pblc-ec2-allow-all-out-sg-policy" {
+  security_group_id = aws_security_group.webserver-pblc-ec2-sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 0
+  ip_protocol       = "-1"
+  to_port           = 65535
+  tags = {
+    Name = "${var.project}-${var.env}-ssh-webserver-ec2-allow-all-out-sg-policy"
+  }
+}
+
+# EC2 in private subnet
+resource "aws_instance" "application-prvt-ec2" {
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = var.instance_type
+  subnet_id                   = var.prvt-environment-subnet
+  associate_public_ip_address = var.enable-prvt-ip-application-ec2
+  security_groups             = [aws_security_group.application-prvt-ec2-sg.id]
+  key_name                    = aws_key_pair.ec2-tf.key_name
+
+  tags = {
+    Name = "${var.project}-${var.env}-application-ec2"
+  }
+}
+
+# SECURITY
+resource "aws_security_group" "application-prvt-ec2-sg" {
+  name        = "${var.project}-${var.env}-application-prvt-ec2-sg"
+  description = "Allow traffic to in and out of EC2."
+  vpc_id      = var.vpc_id
+
+  tags = {
+    Name = "${var.project}-${var.env}-application-prvt-ec2-sg"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "application-prvt-ec2-ssh-sg-policy" {
+  security_group_id = aws_security_group.application-prvt-ec2-sg.id
+  cidr_ipv4         = var.sg-policy-cidr
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+  tags = {
+    Name = "${var.project}-${var.env}-ssh-application-ec2-ssh-sg-policy"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "application-prvt-ec2-allow-all-sg-policy" {
+  security_group_id = aws_security_group.application-prvt-ec2-sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 0
+  ip_protocol       = "-1"
+  to_port           = 65535
+  tags = {
+    Name = "${var.project}-${var.env}-application-ec2-allow-all-sg-policy"
+  }
+}
+
+resource "aws_vpc_security_group_egress_rule" "application-prvt-ec2-allow-all-out-sg-policy" {
+  security_group_id = aws_security_group.application-prvt-ec2-sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 0
+  ip_protocol       = "-1"
+  to_port           = 65535
+  tags = {
+    Name = "${var.project}-${var.env}-application-ec2-allow-all-out-sg-policy"
+  }
+}
--- modules/application/outputs.tf
+++ modules/application/outputs.tf
@@ -3,6 +3,10 @@ output "webserver-ec2-ip" {
 }
 
 output "ssh-key-pem" {
-  value = tls_private_key.my_key.private_key_pem
+  value     = tls_private_key.my_key.private_key_pem
   sensitive = true
+}
+
+output "application-ec2-ip" {
+  value = aws_instance.application-prvt-ec2.private_ip
 }
